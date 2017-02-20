81951b43fd936da8f956b89e5daed1bbc15f2d74
Nikita Loges <git@nikita-loges.de>
2017-02-20 22:47:43 +0100

    dna annotation

---
+++ src/Annotation/DnaAuthorization.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tenolo\Bundle\EntityBundle\Annotation;
+
+/**
+ * Class DnaAuthorization
+ *
+ * @package Tenolo\Bundle\EntityBundle\Annotation
+ * @author  Nikita Loges
+ * @company tenolo GbR
+ *
+ * @Annotation
+ * @Target("METHOD")
+ */
+class DnaAuthorization
+{
+
+    /** @var string */
+    public $className;
+
+    /** @var string */
+    public $paramName;
+
+    /** @var string */
+    public $dna = 'dna';
+
+    public function __construct($options)
+    {
+        if (isset($options['value'])) {
+            $options['paramName'] = $options['value'];
+            unset($options['value']);
+        }
+
+        foreach ($options as $key => $value) {
+            if (!property_exists($this, $key)) {
+                throw new \InvalidArgumentException(sprintf('Property "%s" does not exist', $key));
+            }
+
+            $this->$key = $value;
+        }
+    }
+}
