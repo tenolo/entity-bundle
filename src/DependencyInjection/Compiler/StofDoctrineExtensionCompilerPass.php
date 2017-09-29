f4bad66659ccb3f6af5ae30e33e1809458b2b750
Nikita Loges <nl@tenolo.de>
2017-09-29 16:09:07 +0200

    1.0er release

---
+++ src/DependencyInjection/Compiler/StofDoctrineExtensionCompilerPass.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tenolo\Bundle\EntityBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Tenolo\Bundle\EntityBundle\EventListener\SoftDeleteableListener;
+
+/**
+ * Class StofDoctrineExtensionCompilerPass
+ *
+ * @package Tenolo\Bundle\EntityBundle\DependencyInjection\Compiler
+ * @author  Nikita Loges
+ * @company tenolo GbR
+ */
+class StofDoctrineExtensionCompilerPass implements CompilerPassInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $definition = $container->getDefinition('stof_doctrine_extensions.listener.softdeleteable');
+        $definition->setClass(SoftDeleteableListener::class);
+    }
+
+}
--- src/TenoloEntityBundle.php
+++ src/TenoloEntityBundle.php
@@ -4,9 +4,12 @@ namespace Tenolo\Bundle\EntityBundle;
 
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Mmoreram\SymfonyBundleDependencies\DependentBundleInterface;
+use Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Tenolo\Bundle\EntityBundle\DependencyInjection\Compiler\StofDoctrineExtensionCompilerPass;
 
 /**
  * Class TenoloEntityBundle
@@ -21,11 +24,22 @@ class TenoloEntityBundle extends Bundle implements DependentBundleInterface
     /**
      * @inheritDoc
      */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new StofDoctrineExtensionCompilerPass());
+    }
+
+    /**
+     * @inheritDoc
+     */
     public static function getBundleDependencies(KernelInterface $kernel)
     {
         return [
             FrameworkBundle::class,
             DoctrineBundle::class,
+            StofDoctrineExtensionsBundle::class,
         ];
     }
 }
