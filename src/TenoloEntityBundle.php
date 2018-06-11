a1274e83a7e97db5953cb2cc09e182b667fb32c9
Nikita Loges <git@nikita-loges.de>
2018-06-11 14:49:08 +0200

    bundle dependencies

--- src/TenoloEntityBundle.php
+++ src/TenoloEntityBundle.php
@@ -2,13 +2,8 @@
 
 namespace Tenolo\Bundle\EntityBundle;
 
-use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
-use Mmoreram\SymfonyBundleDependencies\DependentBundleInterface;
-use Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle;
-use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\HttpKernel\KernelInterface;
 use Tenolo\Bundle\EntityBundle\DependencyInjection\Compiler\StofDoctrineExtensionCompilerPass;
 
 /**
@@ -18,7 +13,7 @@ use Tenolo\Bundle\EntityBundle\DependencyInjection\Compiler\StofDoctrineExtensio
  * @author  Nikita Loges
  * @company tenolo GbR
  */
-class TenoloEntityBundle extends Bundle implements DependentBundleInterface
+class TenoloEntityBundle extends Bundle
 {
 
     /**
@@ -29,17 +24,5 @@ class TenoloEntityBundle extends Bundle implements DependentBundleInterface
         parent::build($container);
 
         $container->addCompilerPass(new StofDoctrineExtensionCompilerPass());
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public static function getBundleDependencies(KernelInterface $kernel)
-    {
-        return [
-            FrameworkBundle::class,
-            DoctrineBundle::class,
-            StofDoctrineExtensionsBundle::class,
-        ];
     }
 }
